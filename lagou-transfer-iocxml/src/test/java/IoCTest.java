51c0e3844bd25876fcd6eebdd0eeea90533a96bc
zcming123 <33447327+zcming123@users.noreply.github.com>
2022-08-15 15:04:41 +0800

    纯XML的spring项目demo

---
+++ lagou-transfer-iocxml/src/test/java/IoCTest.java
@@ -0,0 +1,37 @@
+import com.lagou.edu.dao.AccountDao;
+
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+/**
+ * @author 应癫
+ */
+public class IoCTest {
+    @Test
+    public void testIoC() throws Exception {
+
+        // 通过读取classpath下的xml文件来启动容器（xml模式SE应用下推荐）
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+
+        // 不推荐使用
+        //ApplicationContext applicationContext1 = new FileSystemXmlApplicationContext("文件系统的绝对路径");
+
+
+        // 第一次getBean该对象
+        Object accountPojo = applicationContext.getBean("accountPojo");
+
+        AccountDao accountDao = (AccountDao) applicationContext.getBean("accountDao");
+
+        accountDao.queryAccountByCardNo("1111111");
+        System.out.println("accountDao：" + accountDao);
+        AccountDao accountDao1 = (AccountDao) applicationContext.getBean("accountDao");
+        System.out.println("accountDao1：" + accountDao1);
+
+
+        Object connectionUtils = applicationContext.getBean("connectionUtils");
+        System.out.println(connectionUtils);
+
+        applicationContext.close();
+    }
+}
