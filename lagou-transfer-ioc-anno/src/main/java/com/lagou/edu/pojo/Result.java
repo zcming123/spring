3a6505008c94da0e0e162e2ccfe1b1ef374c01a0
zcming123 <33447327+zcming123@users.noreply.github.com>
2022-08-23 15:22:42 +0800

    纯注解的 spring demo

---
+++ lagou-transfer-ioc-anno/src/main/java/com/lagou/edu/pojo/Result.java
@@ -0,0 +1,37 @@
+package com.lagou.edu.pojo;
+
+import org.springframework.context.annotation.Lazy;
+
+/**
+ * @author 应癫
+ */
+//@Lazy
+public class Result {
+
+    private String status;
+    private String message;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                "status='" + status + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
